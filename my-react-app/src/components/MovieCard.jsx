afa7887956c571129b8d9b7696ddb7b90a224b42
Victor Nnamani <victornnamani651@gmail.com>
2025-06-09 03:24:31 +0100

    Built the movie card component

--- my-react-app/src/components/MovieCard.jsx
+++ my-react-app/src/components/MovieCard.jsx
@@ -5,7 +5,7 @@ function MovieCard({ movie }) {
     <div className="movie-card w-25">
       <div className="movie-poster">
         <img src={movie.url} alt={movie.title} className="img-fluid" />
-        <button className="favorite-btn">
+        <button className="favorite-btn btn btn-success">
           <i className="bi bi-heart"></i>
         </button>
       </div>
