249830e225828086342acdff4ab6ecff47073261
Victor Nnamani <victornnamani651@gmail.com>
2025-07-08 11:08:07 -0700

    Added Loading Screen & Currently Still Building Favorites Feature

--- my-react-app/src/components/MovieCard.jsx
+++ my-react-app/src/components/MovieCard.jsx
@@ -1,12 +1,16 @@
 import "./MovieCard.css";
 
-function MovieCard({ movie }) {
+function MovieCard({ movie, favorite, toggleFavorite }) {
   return (
-    <div className="movie-card col-5 col-md-auto">
+    <div className="movie-card col-auto col-md-3 d-flex flex-column justify-content-center align-items-center">
       <div className="movie-poster">
-        <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title} className="img-fluid" />
-        <button className="favorite-btn btn btn-success">
-          <i className="bi bi-heart"></i>
+        <img
+          src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`}
+          alt={movie.title}
+          className="img-fluid d-block"
+        />
+        <button className="favorite-btn btn btn-warning">
+          <i className={`bi ${favorite} fs-5`}></i>
         </button>
       </div>
       <div className="movie-info">
--- my-react-app/src/pages/Home.jsx
+++ my-react-app/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import MovieCard from "../components/MovieCard";
 import { useEffect, useState } from "react";
 import { getPopularMovies } from "../services/api";
+import "./home.css";
 
 function Home({ search_query_length, search_query }) {
   const [movies, setMovies] = useState([]);
   const [err, setErr] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [favorite, setFavorite] = useState("bi-heart");
 
   useEffect(() => {
     const loadPopularMovies = async () => {
@@ -14,7 +16,7 @@ function Home({ search_query_length, search_query }) {
         setMovies(popularMovies);
       } catch (err) {
         setErr("Failed To Load Movies");
-        console.log(err);
+        console.error(err);
       } finally {
         setLoading(false);
       }
@@ -23,20 +25,40 @@ function Home({ search_query_length, search_query }) {
   }, [movies]);
 
   return (
-    <div className="container-md border border-3 border-danger p-0">
-      <div className="movie-container border border-2 border-info row justify-content-center m-0 py-2">
+    <div className="container-md p-0">
+      <div className="movie-container row justify-content-center m-0 py-2">
         {/* i set a conditional render for this component where all the movies are displayed when there is nothing typed into the search bar and when there is, only the movies with titles that contain the letters or words that were typed should be displayed and if there are no movies matching the inputted letter or words, nothing should be displayed */}
 
-        {movies.map((movie) => {
-          if (
-            search_query_length > 0 &&
-            movie.title.toLowerCase().startsWith(search_query)
-          ) {
-            return <MovieCard movie={movie} key={movie.id} />;
-          } else if (search_query_length == 0) {
-            return <MovieCard movie={movie} key={movie.id} />;
-          }
-        })}
+        {loading ? (
+          <div className="loading d-flex align-items-center justify-content-center fw-bolder fs-1">
+            Loading...
+          </div>
+        ) : (
+          movies.map((movie) => {
+            if (
+              search_query_length > 0 &&
+              movie.title.toLowerCase().startsWith(search_query)
+            ) {
+              return (
+                <MovieCard
+                  movie={movie}
+                  key={movie.id}
+                  favorite={favorite}
+                  toggleFavorite={() => setFavorite("bi-heart-fill")}
+                />
+              );
+            } else if (search_query_length == 0) {
+              return (
+                <MovieCard
+                  movie={movie}
+                  key={movie.id}
+                  favorite={favorite}
+                  toggleFavorite={() => setFavorite("bi-heart-fill")}
+                />
+              );
+            }
+          })
+        )}
       </div>
     </div>
   );
